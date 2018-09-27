3a03b54e82ebb4dd82545a06059cc17d7897eebe
nadiacoleman <nadia.coleman@ocsbstudent.ca>
2018-09-27 14:25:49 -0400

    Initial commit

---
+++ DisplayShapesNadia/main.lua
@@ -0,0 +1,18 @@
+-- Title: Display Shapes
+-- Name: Nadia Coleman
+-- Course: ICS2O
+-- This program displays shapes and their names under them on the Ipad 
+-----------------------------------------------------------------------------------------
+
+local halfW = display.contentWidth * 1
+local halfH = display.contentHeight * 1
+ 
+local vertices = { 0,-300, 0,-95, 99,-35, 43,0 }
+ 
+local Shape1 = display.newPolygon( halfW, halfH, vertices )
+Shape1.fill = { type="image", filename="mountains.png" }
+Shape1.strokeWidth = 10
+Shape1:setStrokeColor( 1, 0, 0 )
+
+Shape1.x = 500
+Shape1.y = 500
