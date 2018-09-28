84cc1ea454e65d337c14a9758ff55c665cd8880e
nadiacoleman <nadia.coleman@ocsbstudent.ca>
2018-09-28 13:58:01 -0400

    Changed fill colour

--- DisplayShapesNadia/main.lua
+++ DisplayShapesNadia/main.lua
@@ -3,10 +3,18 @@
 -- Course: ICS2O
 -- This program displays shapes and their names under them on the Ipad 
 -----------------------------------------------------------------------------------------
- 
+
+-- Create local variable for vertices. 
 local vertices = { 50,-300, 6,-95, 99,-35}
  
-local triangle = display.newTriangle( 100, 150, vertices )
-triangle.fillColor = (0, 1, 0)
+-- Create new shape, a triangle.
+local triangle = display.newPolygon( 100, 150, vertices )
+
+-- Set fill colour of triangle
+triangle:setFillColor(0.15, 0.25, 0.50)
+
+-- Set stroke width of triangle's border
 triangle.strokeWidth = 10
-triangle:setStrokeColor = (1, 0, 0)
+
+-- Set stroke colour
+triangle:setStrokeColor(1, 0, 0)
