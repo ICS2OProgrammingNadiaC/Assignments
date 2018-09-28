e3ded6486d749b84506d44eff2445cbee317df2b
nadiacoleman <nadia.coleman@ocsbstudent.ca>
2018-09-28 13:24:37 -0400

    Added space

--- DisplayShapesNadia/main.lua
+++ DisplayShapesNadia/main.lua
@@ -10,9 +10,9 @@ local halfH = display.contentHeight * 1
 local vertices = { 50,-300, 6,-95, 99,-35}
  
 local Shape1 = display.newPolygon( halfW, halfH, vertices )
-Shape1.fill = { type="image", filename="mountains.png" }
+Shape1.fill = (0, 1, 0)
 Shape1.strokeWidth = 10
-Shape1:setStrokeColor( 1, 0, 0 )
+Shape1:setStrokeColor(1, 0, 0)
 
 Shape1.x = 100
 Shape1.y = 150
