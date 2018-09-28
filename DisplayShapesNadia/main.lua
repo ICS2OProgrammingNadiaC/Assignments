2f5be2580db37a7481f0a556e720dc1fc153d50f
nadiacoleman <nadia.coleman@ocsbstudent.ca>
2018-09-28 13:47:05 -0400

    Added more code

--- DisplayShapesNadia/main.lua
+++ DisplayShapesNadia/main.lua
@@ -3,16 +3,10 @@
 -- Course: ICS2O
 -- This program displays shapes and their names under them on the Ipad 
 -----------------------------------------------------------------------------------------
-
-local halfW = display.contentWidth * 1
-local halfH = display.contentHeight * 1
  
 local vertices = { 50,-300, 6,-95, 99,-35}
  
-local Shape1 = display.newPolygon( halfW, halfH, vertices )
-Shape1.fill = (0, 1, 0)
-Shape1.strokeWidth = 10
-Shape1:setStrokeColor(1, 0, 0)
-
-Shape1.x = 100
-Shape1.y = 150
+local triangle = display.newTriangle( 100, 150, vertices )
+triangle.fillColor = (0, 1, 0)
+triangle.strokeWidth = 10
+triangle:setStrokeColor = (1, 0, 0)
