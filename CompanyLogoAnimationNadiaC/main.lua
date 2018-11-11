b53bfc077d061eabd981ef25691db9152f8a67e3
Nadia <nadia.coleman@ocsbstudent.ca>
2018-11-11 13:00:58 -0800

    Update CompanyLogoAnimationNadiaC/main.lua

--- CompanyLogoAnimationNadiaC/main.lua
+++ CompanyLogoAnimationNadiaC/main.lua
@@ -2,13 +2,25 @@
 -- Course: ICS20
 -- By: Nadia Coleman
 -- this program animates the company logo
-----------------------------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------
+-- TEXT
+--------------------------------------------------------------------------------------------
+-- create text object
+local textObject
+
+-- displays text on the screen at posistion x = 500 and y = 500 with 
+-- a default font style and font size of 50
+textObject = display.newText( "Pyramid Software", 500, 300, nil, 100)
+
+-- sets the color of the text
+textObject:setTextColor(155/255, 42/255, 198/255)
+
+--------------------------------------------------------------------------------------------
 
 -- hide the status bar
-<<<<<<< HEAD
 display.setStatusBar(display.HiddenStatusBar)
-=======
-display.setStatusBar(display.HiddenStatusBar)
+
 
 -- gloabal variables
 scrollSpeed = 3
@@ -23,11 +35,21 @@ companyLogo.x = 500
 -- set the company logo Y
 companyLogo.y = 500
 
+-- set the company logo to be transparent
+companyLogo.alpha = 0
+
+
 
 -- make the logo spin and rotate
 transition.to(companyLogo, {rotation=360, time=3000, onComplete=spinImage})
-transition.to(companyLogo, {x=500, y=500, time=3000})
+transition.to(companyLogo, {x=500, y=500, time=3000, onComplete=rotateImage})
 
+-- Function: MoveCompanyLogo
+-- Input: this function accepts an event listener
+local function MoveCompanyLogo(event)
+	-- change the transparency of the image
+	companyLogo.alpha = companyLogo.alpha + 0.01
+end
 
 -- Function: ScaleCompanyLogo
 -- Input: this function also accepts an event listener
@@ -39,7 +61,10 @@ end
 
 -- call the companyLogo again and again
 Runtime:addEventListener("enterFrame", ScaleCompanyLogo)
+-- MoveCompanyLogo will be called again and again
+Runtime:addEventListener("enterFrame", MoveCompanyLogo)
 
+-- cancel animation
 
 --------------------------------------------------------------------------------------------
 -- SOUNDS
@@ -49,4 +74,3 @@ Runtime:addEventListener("enterFrame", ScaleCompanyLogo)
 local backgroundSound = audio.loadSound( "Sounds/background.mp3" )
 local backgroundSoundChannel
 backgroundSoundChannel = audio.play(backgroundSound)
->>>>>>> 5aa63e09e2c3d17b38ae1b1883fab251e5da7d7a
