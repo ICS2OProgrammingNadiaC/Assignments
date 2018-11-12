5516a9de2e1eb6d3576d354509f0f014bdb1da2d
nadiacoleman <nadia.coleman@ocsbstudent.ca>
2018-11-12 14:24:42 -0500

    Update CompanyLogoAnimationNadiaC/main.lua

--- CompanyLogoAnimationNadiaC/main.lua
+++ CompanyLogoAnimationNadiaC/main.lua
@@ -2,25 +2,10 @@
 -- Course: ICS20
 -- By: Nadia Coleman
 -- this program animates the company logo
----------------------------------------------------------------------------------------------------------------------
---------------------------------------------------------------------------------------------
--- TEXT
---------------------------------------------------------------------------------------------
--- create text object
-local textObject
-
--- displays text on the screen at posistion x = 500 and y = 500 with 
--- a default font style and font size of 50
-textObject = display.newText( "Pyramid Software", 500, 300, nil, 100)
-
--- sets the color of the text
-textObject:setTextColor(155/255, 42/255, 198/255)
-
---------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------
 
 -- hide the status bar
 display.setStatusBar(display.HiddenStatusBar)
-
 
 -- gloabal variables
 scrollSpeed = 3
@@ -35,21 +20,11 @@ companyLogo.x = 500
 -- set the company logo Y
 companyLogo.y = 500
 
--- set the company logo to be transparent
-companyLogo.alpha = 0
-
-
 
 -- make the logo spin and rotate
 transition.to(companyLogo, {rotation=360, time=3000, onComplete=spinImage})
-transition.to(companyLogo, {x=500, y=500, time=3000, onComplete=rotateImage})
+transition.to(companyLogo, {x=500, y=500, time=3000})
 
--- Function: MoveCompanyLogo
--- Input: this function accepts an event listener
-local function MoveCompanyLogo(event)
-	-- change the transparency of the image
-	companyLogo.alpha = companyLogo.alpha + 0.01
-end
 
 -- Function: ScaleCompanyLogo
 -- Input: this function also accepts an event listener
@@ -61,10 +36,7 @@ end
 
 -- call the companyLogo again and again
 Runtime:addEventListener("enterFrame", ScaleCompanyLogo)
--- MoveCompanyLogo will be called again and again
-Runtime:addEventListener("enterFrame", MoveCompanyLogo)
 
--- cancel animation
 
 --------------------------------------------------------------------------------------------
 -- SOUNDS
